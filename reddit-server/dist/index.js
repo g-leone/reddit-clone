ffacdc47e22e14d72ecaf1a93cd103a1155251fa
g-leone <garrett.leone@gmail.com>
2020-09-11 11:25:03 -0700

    grapql update

--- reddit-server/dist/index.js
+++ reddit-server/dist/index.js
@@ -12,14 +12,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
 const core_1 = require("@mikro-orm/core");
-const post_1 = require("./Entities/post");
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const express_1 = __importDefault(require("express"));
+const apollo_server_express_1 = require("apollo-server-express");
+const type_graphql_1 = require("type-graphql");
+const hello_1 = require("./resolvers/hello");
+const post_1 = require("./resolvers/post");
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     const orm = yield core_1.MikroORM.init(mikro_orm_config_1.default);
     orm.getMigrator().up();
-    const post = orm.em.create(post_1.Post, { title: "my first post" });
-    yield orm.em.persistAndFlush(post);
+    const app = express_1.default();
+    const appolloServer = new apollo_server_express_1.ApolloServer({
+        schema: yield type_graphql_1.buildSchema({
+            resolvers: [hello_1.HelloResolver, post_1.PostResolver],
+            validate: false,
+        }),
+        context: () => ({ em: orm.em }),
+    });
+    appolloServer.applyMiddleware({ app });
+    app.listen(4444, () => {
+        console.log("server started on localhost:4444");
+    });
 });
 main().catch((err) => {
     console.error(err);
--- reddit-server/src/index.ts
+++ reddit-server/src/index.ts
@@ -1,14 +1,37 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./Entities/post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   orm.getMigrator().up();
 
-  const post = orm.em.create(Post, { title: "my first post" });
-  await orm.em.persistAndFlush(post);
+  const app = express();
+
+  const appolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  appolloServer.applyMiddleware({ app });
+
+  app.listen(4444, () => {
+    console.log("server started on localhost:4444");
+  });
+  // const post = orm.em.create(Post, { title: "my first post" });
+  // await orm.em.persistAndFlush(post);
+
+  // const post = await orm.em.find(Post, {});
+  // console.log(post);
 };
 
 main().catch((err) => {
